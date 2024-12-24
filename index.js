d66e4e8506ec7f49dbbf92b21c9139f58faa524c
Jakaria030 <gulamjakaria99@gmail.com>
2024-12-24 16:04:39 +0600

    Added get api to read single room

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-
-const DB_USER='seaHaven'
-const DB_PASS='F0K7JI7PNuJPdEFS'
+app.use(cors());
+app.use(express.json());
 
 // mongodb connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7vwvj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -41,6 +40,14 @@ async function run() {
       res.send(result);
     });
 
+    // get specific room
+    app.get('/rooms/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await roomsCollection.findOne(query);
+
+      res.send(result);
+    });
     // rooms releated api end
 
 
